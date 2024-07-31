0433d4caf12b8d3d2dbe21867df22c09747ee076
Itallo Ferreira do Nascimento <itallo.ferreira@academico.ifrn.edu.br>
2024-07-31 17:25:43 -0300

    .

--- Caminhos do orgulho - Vivencias e desafios LGBTQ+/Assets/Scenes/Scripts/Player.cs
+++ Caminhos do orgulho - Vivencias e desafios LGBTQ+/Assets/Scenes/Scripts/Player.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Ground Properties")] 
+    public LayerMask groundLayer;
+
+    public float groundDistance;
+    public bool isGrounded;
+    public Vector3[] footOffset;
 
     public float speed = 2f;
-
+    public float jumpForce = 2f;
     private Rigidbody2D rb2d;
-
     private Vector2 movement;
-
     private float xVelocity;
 
     private int direction =1;
-
     private float originalXScale;
+
+    RaycastHit2D leftCheck;
+    RaycastHit2D rightCheck;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +40,8 @@ public class Player : MonoBehaviour
         {
             Flip();
         }
+        
+        
     }
 
     private void FixedUpdate()
@@ -49,6 +57,30 @@ public class Player : MonoBehaviour
 
         scale.x = originalXScale * direction;
         transform.localScale = scale;
+    }
+
+    private void PhysicCheck()
+    {
+        isGrounded = false;
+        leftCheck = Raycast(new Vector2(footOffset[0].x, footOffset[0].y), Vector2.down, groundDistance, groundLayer);
+        Vector2.down, groundDistance, groundLayer);
+       rightCheck = Raycast(new Vector2(footOffset[0].x, footOffset[0].y), Vector2.down, groundDistance, groundLayer);
+       Vector2.down, groundDistance, groundLayer);
+       
+       if (leftCheck || rightCheck)
+       {
+           isGrounded = true;
+       }
+       
+    }
+
+    private RaycastHit2D Raycast(Vector3 origin, Vector2 rayDirection, float length, LayerMask mask)
+    {
+        Vector3 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(pos + origin, rayDirection, length, mask);
+        Color color = hit ? Color.red : Color.green;
+        Debug.DrawRay(pos + origin, rayDirection * length, color);
+        return hit;
     }
 }
   
