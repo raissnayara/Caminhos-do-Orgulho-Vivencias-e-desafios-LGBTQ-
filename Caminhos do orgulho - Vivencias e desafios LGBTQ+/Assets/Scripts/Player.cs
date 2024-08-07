d062185dfd7d8c005b1e22170a3d4893b2153e98
Itallo Ferreira do Nascimento <itallo.ferreira@academico.ifrn.edu.br>
2024-08-07 09:28:56 -0300

    codigos de pulo e movimentaçao ajeitados

--- Caminhos do orgulho - Vivencias e desafios LGBTQ+/Assets/Scripts/Player.cs
+++ Caminhos do orgulho - Vivencias e desafios LGBTQ+/Assets/Scripts/Player.cs
@@ -5,86 +5,74 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("Ground Properties")] 
-    public LayerMask groundLayer;
+    public float speed;
+    public float jumpForce;
+    private bool isJumping;
+    private bool doubleJump;
 
-    public float groundDistance;
-    public bool isGrounded;
-    public Vector3[] footOffset;
-
-    public float speed = 2f;
-    public float jumpForce = 2f;
-    private Rigidbody2D rb2d;
-    private Vector2 movement;
-    private float xVelocity;
-
-    private int direction =1;
-    private float originalXScale;
-
-    RaycastHit2D leftCheck;
-    RaycastHit2D rightCheck;
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rig;
+    private Animator anim;
+    private void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        originalXScale = transform.localScale.x;
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        movement = new Vector2(horizontal, 0);
+        Move();
+        Jump();
+    }
 
-        if (xVelocity * direction < 0)
+    void Move()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(movement * speed, rig.velocity.y);
+
+        if (movement > 0)
         {
-            Flip();
+            transform.eulerAngles = new Vector3(0,0,0);
         }
 
-        PhysicCheck();
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (movement < 0)
         {
-            rb2d.velocity = Vector2.zero;
-            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            transform.eulerAngles = new Vector3(0,180,0);
         }
 
+        
+    }
+    
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!isJumping)
+            {
+                rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                isJumping = true;
+                doubleJump = true;
+            }
+
+            else
+            {
+                if (doubleJump)
+                {
+                    rig.AddForce(new Vector2(0, jumpForce * 2), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+            
+        }
     }
 
-    private void FixedUpdate()
+    void OnCollisionEnter2D(Collision2D coll)
     {
-        xVelocity = movement.normalized.x * speed;
-        rb2d.velocity = new Vector2(xVelocity, rb2d.velocity.y);
-    }
-
-    private void Flip()
-    {
-        direction *= -1;
-        Vector3 scale = transform.localScale;
-
-        scale.x = originalXScale * direction;
-        transform.localScale = scale;
-    }
-
-    private void PhysicCheck()
-    {
-        isGrounded = false;
-        leftCheck = Raycast(new Vector2(footOffset[0].x, footOffset[0].y), Vector2.down, groundDistance, groundLayer);
-       rightCheck = Raycast(new Vector2(footOffset[0].x, footOffset[0].y), Vector2.down, groundDistance, groundLayer);
-       
-       if (leftCheck || rightCheck)
-       {
-           isGrounded = true;
-       }
-       
-    }
-
-    private RaycastHit2D Raycast(Vector3 origin, Vector2 rayDirection, float length, LayerMask mask)
-    {
-        Vector3 pos = transform.position;
-        RaycastHit2D hit = Physics2D.Raycast(pos + origin, rayDirection, length, mask);
-        Color color = hit ? Color.red : Color.green;
-        Debug.DrawRay(pos + origin, rayDirection * length, color);
-        return hit;
+        if (coll.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
     }
 }
-  
+    
+    
+          
